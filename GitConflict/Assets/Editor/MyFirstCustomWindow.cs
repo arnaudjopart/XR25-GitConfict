c69addce804b8541bab36ad4cb2cbcb93f63c999
Arnaud <arnaud.jopart@gmail.com>
2025-06-13 16:49:00 +0200

    wip

--- GitConflict/Assets/Editor/MyFirstCustomWindow.cs
+++ GitConflict/Assets/Editor/MyFirstCustomWindow.cs
@@ -1,11 +1,13 @@
 using Unity.VisualScripting;
 using UnityEditor;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class MyFirstCustomWindow : EditorWindow
 {
     private Label _label;
+    private Label _nbOfGameObjects;
 
     [MenuItem("XR25/Tools/My First Custom Window")]
     public static void ShowExample()
@@ -21,11 +23,25 @@ public class MyFirstCustomWindow : EditorWindow
 
         // VisualElements objects can contain other VisualElement following a tree hierarchy.
         _label = new Label("Name of Game Object ?");
-        Button sayHelloButton = new Button
+        Button deleteGameObjectButton = new Button
         {
-            text = "Tell me your name"
+            text = "Delete Me"
         };
-        sayHelloButton.clicked += OnClick;
+        deleteGameObjectButton.clicked += () =>
+        {
+            if (Selection.activeGameObject != null)
+            {
+                var objectsToDelete = Selection.gameObjects;
+                foreach (var obj in objectsToDelete)
+                {
+                    Undo.DestroyObjectImmediate(obj);
+                }
+            }
+            else
+            {
+                Debug.Log("No Object Selected");
+            }
+        };
 
         Button RandomRotateButton = new Button();
         RandomRotateButton.text = "Random Rotation";
@@ -42,18 +58,66 @@ public class MyFirstCustomWindow : EditorWindow
             }
         };
         
-        root.Add(sayHelloButton);
+        
+        Button createPrefabButton = new Button
+        {
+            text = "Create Prefab"
+        };
+        createPrefabButton.clicked += CreatePrefabButton;
+        
+        Button getSceneInfoButton = new Button
+        {
+            text = "Get Scene Info"
+        };
+        createPrefabButton.clicked += GetSceneInfo;
+
+        _nbOfGameObjects = new Label("Nb of GameObjects ?");
+        root.Add(deleteGameObjectButton);
         root.Add(_label);
         root.Add(RandomRotateButton);
-
+        root.Add(createPrefabButton);
+        root.Add(getSceneInfoButton);
+        
+        
     }
 
-    private void OnClick()
+    private void GetSceneInfo()
+    {
+        var currentScene = EditorSceneManager.GetActiveScene();
+        var nbOfRootGAmeObject = currentScene.rootCount;
+        //_nbOfGameObjects.text = $"Number of GameObject in Scene" nbOfRootGAmeObject.ToString();
+    }
+
+    private void CreatePrefabButton()
+    {
+
+        if (Selection.activeGameObject != null)
+        {
+           GameObject currentGamerObject = Selection.activeGameObject;
+           if (!currentGamerObject.TryGetComponent(out TestScript testScript))
+           {
+               currentGamerObject.AddComponent<TestScript>();
+           }
+
+           PrefabUtility.SaveAsPrefabAsset(
+               currentGamerObject, "Assets/My Prefabs/My Prefab.prefab", out bool success);
+           if(success) Debug.Log("Success");
+        }
+        else
+        {
+            Debug.Log("No Object Selected");
+        }
+    }
+
+    private void DestroySelectedGameObject()
     {
         if (Selection.activeGameObject != null)
         {
-            var meshRenderer = Selection.activeGameObject.GetComponent<MeshRenderer>();
-            meshRenderer.sharedMaterial.SetColor("_BaseColor", Color.white);
+            GameObject[] objectsToDelete = Selection.gameObjects;
+            foreach (GameObject obj in objectsToDelete)
+            {
+                Undo.DestroyObjectImmediate(obj);
+            }
         }
         else
         {
